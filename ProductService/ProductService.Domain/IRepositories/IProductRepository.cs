9b91bb73dc7ee6e9625d46c9ee0ab26414fc5f75
Mahmudur Rahman <mahmudurrahman171@gmail.com>
2026-02-07 13:18:59 +0600

    refactor: remove unused methods from product service
    Removed unused methods from the product service in the ProductService.

--- ProductService/ProductService.Domain/IRepositories/IProductRepository.cs
+++ ProductService/ProductService.Domain/IRepositories/IProductRepository.cs
@@ -4,6 +4,4 @@ namespace ProductService.Domain.IRepositories;
 
 public interface IProductRepository : IRepository<Product>
 {
-    Task<List<Product>> GetAllProductsAsync(string? name, bool isActive, int itemsPerPage, int pageNumber);
-    Task<int> GetAllProductCountAsync(string? name, bool isActive);
 }
